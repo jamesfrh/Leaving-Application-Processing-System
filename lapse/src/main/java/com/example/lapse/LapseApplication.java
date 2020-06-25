8e620dc3ad4fd8e2dde523a4d68a8c7e5ea2d288
C H Leng <simply.90@gmail.com>
2020-06-25 21:16:05 +0800

    Test commit

--- lapse/src/main/java/com/example/lapse/LapseApplication.java
+++ lapse/src/main/java/com/example/lapse/LapseApplication.java
@@ -1,7 +1,10 @@
 package com.example.lapse;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LapseApplication {
@@ -9,5 +12,12 @@ public class LapseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LapseApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Hello World");
+		};
+	}
 }
+

8ee2a0d42bbc44a38d7ad2301eccef293ad52a2f
jamesfrh <james.frh@gmail.com>
2020-06-25 20:55:50 +0800

    First Commit

---
+++ lapse/src/main/java/com/example/lapse/LapseApplication.java
@@ -0,0 +1,13 @@
+package com.example.lapse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapseApplication.class, args);
+	}
+
+}
